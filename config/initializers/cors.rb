bf708feb6264fa1630dc68adb14e996ab3a03baf
Joshua McGehee <mcgehee.joshua@gmail.com>
2019-08-11 11:28:14 -0600

    edited cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,7 +15,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'jmmcgehee.github.io/PlowShare_frontend'
+    origins 'https://jmmcgehee.github.io'
 
     resource '*',
       headers: :any,
